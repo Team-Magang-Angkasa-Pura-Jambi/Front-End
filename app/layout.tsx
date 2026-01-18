cbf207a8dc75132014a6495402998997a24e2eeb
Hychtclyff <yudriqul20803@gmail.com>
2026-01-18 15:30:14 +0700

    feat(testing): add testing script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css';
+import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
@@ -6,6 +6,8 @@ import { QueryProvider } from "@/providers/QueryProvider";
 import { Toaster } from "@/common/components/ui/sonner";
 import { SocketProvider } from "@/providers/SocketProvider";
 import { cn } from "@/lib/utils";
+// 1. IMPORT Script DARI NEXT.JS
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,11 +36,8 @@ export default function RootLayout({
           geistSans.variable,
           geistMono.variable,
           "min-h-screen antialiased",
-
           "bg-background text-foreground",
-
           "selection:bg-primary/20 selection:text-primary",
-
           "[&::-webkit-scrollbar]:w-2",
           "[&::-webkit-scrollbar-track]:bg-transparent",
           "[&::-webkit-scrollbar-thumb]:bg-slate-300 dark:[&::-webkit-scrollbar-thumb]:bg-slate-700",
@@ -46,6 +45,39 @@ export default function RootLayout({
           "[&::-webkit-scrollbar-thumb]:border-[1px] [&::-webkit-scrollbar-thumb]:border-transparent [&::-webkit-scrollbar-thumb]:bg-clip-content"
         )}
       >
+        {/* 2. GUNAKAN KOMPONEN SCRIPT DI SINI */}
+        {/* strategy="afterInteractive" memastikan script load setelah halaman interaktif agar tidak bikin lemot */}
+        <Script id="maze-snippet" strategy="afterInteractive">
+          {`
+            (function (m, a, z, e) {
+              var s, t, u, v;
+              try {
+                t = m.sessionStorage.getItem('maze-us');
+              } catch (err) {}
+            
+              if (!t) {
+                t = new Date().getTime();
+                try {
+                  m.sessionStorage.setItem('maze-us', t);
+                } catch (err) {}
+              }
+            
+              u = document.currentScript || (function () {
+                var w = document.getElementsByTagName('script');
+                return w[w.length - 1];
+              })();
+              v = u && u.nonce;
+            
+              s = a.createElement('script');
+              s.src = z + '?apiKey=' + e;
+              s.async = true;
+              if (v) s.setAttribute('nonce', v);
+              a.getElementsByTagName('head')[0].appendChild(s);
+              m.mazeUniversalSnippetApiKey = e;
+            })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'd09e62d0-cf10-4f62-a1cb-61f1c7fc87f1');
+          `}
+        </Script>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
