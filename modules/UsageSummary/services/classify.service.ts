e9beab1bc4fa6cf4be4d3bd6b54b66bb2784ae8d
Hychtclyff <yudriqul20803@gmail.com>
2026-01-31 11:31:21 +0700

    fix: remove unused apiResponse in classify service

--- modules/UsageSummary/services/classify.service.ts
+++ modules/UsageSummary/services/classify.service.ts
@@ -1,12 +1,7 @@
 import api from "@/lib/api";
 import { SingleAnalysisPayload } from "../types/recap.type";
-import { ApiResponse } from "@/common/types/api";
 
 const CLASSIFY_PREFIX = "/classify";
-
-// ==========================================
-// 1. CLASSIFICATION SERVICES
-// ==========================================
 
 export const classifiesApi = async (payload: SingleAnalysisPayload) => {
   const { data } = await api.post(`${CLASSIFY_PREFIX}`, payload);
